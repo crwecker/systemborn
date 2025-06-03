405ec0cf30a3fa52475f71a7f98ed87afd643db4
Carl Wecker <crwecker@gmail.com>
2025-06-02 22:23:53 -0500

    add vite netlifyplugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import { reactRouter } from '@react-router/dev/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import netlifyPlugin from '@netlify/vite-plugin-react-router';
 
 export default defineConfig({
   plugins: [
     reactRouter(),
     tsconfigPaths(),
+    netlifyPlugin(),
   ],
   build: {
     commonjsOptions: {
